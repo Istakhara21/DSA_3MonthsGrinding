259ec1eabb67d5550703b88ad1510b7ec721795a
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-11 15:55:52 +0530

    Solved lots of pattern questions..

--- code.cpp
+++ code.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    int b = --a; // 9,9
-
-    cout << b << endl; // b me pehle a ki value jayegi
-    cout << a; // ab a update ho chuka hai
+    int n = 0;
+    cin >> n;
+    char ch = 'A';
+    int num = 1;
+    for(int i=0; i<n; i++) {
+        char ch = ' ';
+        for(int j=1; j<=n; j++) {
+            
+            for(int k=0; k<i; k++) {
+                cout << ch;
+            }
+            cout << i+1;
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
