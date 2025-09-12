a1b8918b7ee177966be2b243bc5c9d1edfd4c3c5
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-12 15:21:26 +0530

    solved some pattern problems

--- code.cpp
+++ code.cpp
@@ -4,19 +4,42 @@ using namespace std;
 int main() {
     int n = 0;
     cin >> n;
-    char ch = 'A';
-    int num = 1;
+
+
     for(int i=0; i<n; i++) {
-        char ch = ' ';
-        for(int j=1; j<=n; j++) {
-            
-            for(int k=0; k<i; k++) {
-                cout << ch;
+
+        // this is for one side
+        for(int j=0; j<n-1-i; j++) {
+            cout << ' ';
+        }
+        cout << '*';
+
+        if(i !=0) {
+            for(int j=0; j<2*i-1; j++) {
+                cout << ' ';
             }
-            cout << i+1;
+        cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<i+1; j++) {
+            cout << ' ';
+        }
+        cout << '*';
+        if(i != n-1) {
+            for(int j =n-2-i; j>0; j--) {
+            cout << '!';
+        }
+        cout << '*';
         }
         cout << endl;
     }
-    
+
     return 0;
-}
+
+    }
+    
+    
