bd3c225bfa8a97e8b927bd3f758c37daccbd1bc1
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-14 11:29:11 +0530

    Hollow daimont pattern

--- code.cpp
+++ code.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n = 0;
     cin >> n;
 
-
-    for(int i=0; i<n; i++) {
-
-        // this is for one side
-        for(int j=0; j<n-1-i; j++) {
-            cout << ' ';
+    // Fo Top half
+    for (int i = 0; i < n; i++)
+    {
+        // first space
+        for (int j = n - 1 - i; j > 0; j--)
+        {
+            cout << " ";
         }
         cout << '*';
 
-        if(i !=0) {
-            for(int j=0; j<2*i-1; j++) {
+        //odd number of spaces (2n-1)
+        if (i != 0)
+        {
+            for (int j=0; j<(2*i-1); j++)
+            {
                 cout << ' ';
             }
-        cout << '*';
+            cout << '*';
         }
 
         cout << endl;
     }
 
+    //for Bottom half
     for(int i=0; i<n-1; i++) {
         for(int j=0; j<i+1; j++) {
             cout << ' ';
         }
         cout << '*';
-        if(i != n-1) {
-            for(int j =n-2-i; j>0; j--) {
-            cout << '!';
+        for(int j=(2*(n-i)-5); j>0; j--) {
+            cout << ' ';
         }
+        if( i<n-2) {
         cout << '*';
         }
         cout << endl;
     }
 
     return 0;
-
-    }
-    
-    
+}
