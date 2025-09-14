91fffe79f1100064dd79a444adf7885ee8fbf972
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-14 11:43:22 +0530

    butterfly pattern

--- code.cpp
+++ code.cpp
@@ -6,40 +6,32 @@ int main()
     int n = 0;
     cin >> n;
 
-    // Fo Top half
-    for (int i = 0; i < n; i++)
-    {
-        // first space
-        for (int j = n - 1 - i; j > 0; j--)
-        {
-            cout << " ";
-        }
-        cout << '*';
+    //For Top Half
 
-        //odd number of spaces (2n-1)
-        if (i != 0)
-        {
-            for (int j=0; j<(2*i-1); j++)
-            {
-                cout << ' ';
-            }
+    //first triangle
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<=i; j++) {
             cout << '*';
         }
-
+        for(int j=2*(n-i)-2; j>0; j--) {
+            cout << ' ';
+        }
+        for(int j=0; j<i+1; j++) {
+            cout << '*';
+        }
         cout << endl;
     }
 
-    //for Bottom half
-    for(int i=0; i<n-1; i++) {
-        for(int j=0; j<i+1; j++) {
-            cout << ' ';
+    //For Bottom Half
+    for(int i=0; i<n; i++) {
+        for(int j=n-i; j>0; j--) {
+            cout << '*';
         }
-        cout << '*';
-        for(int j=(2*(n-i)-5); j>0; j--) {
-            cout << ' ';
+        for(int j=0; j<2*i; j++) {
+            cout << " ";
         }
-        if( i<n-2) {
-        cout << '*';
+        for(int j=n-i; j>0; j--){
+            cout << '*';
         }
         cout << endl;
     }
