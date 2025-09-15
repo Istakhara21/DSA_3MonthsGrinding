9876ad35d6a608a2477a3cea9fa08ca5154e02ec
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-15 13:48:54 +0530

    Decimal to Binary

--- code.cpp
+++ code.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-// to write nth term of fibonacci series
-int fibo(int n)
-{
-    int a = 0, b = 1;
-    int next = a + b;
-
-    // cout << a << "  " << b << "  ";
-    for (int i = 0; i < n - 2; i++)
-    {
-        next = a + b;
-
-        a = b;
-        b = next;
-        // cout << next << "  ";
+// Decimal to Binary
+int DecToBin(int n) {
+    int ans = 0; // binary num
+    int pow = 1; // (10)0
+    while(n>0) {
+        int rem = n%2;
+        n = n/2;
+        ans = ans + (rem*pow);
+        pow = pow*10;
     }
-    return next;
+    return ans;
 }
 
 int main()
 {
 
-    cout << fibo(10);
+    cout << DecToBin(100);
     return 0;
 };
