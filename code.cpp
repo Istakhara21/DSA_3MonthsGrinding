ec2aab22dc9c236ea97246c0678687ff8efbb085
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-15 12:51:42 +0530

    write nth term of fibonacci series

--- code.cpp
+++ code.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// to check whether a number is prime or not
-bool isPrime(int n)
+// to write nth term of fibonacci series
+int fibo(int n)
 {
-    if (n <= 1)
-    {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-};
+    int a = 0, b = 1;
+    int next = a + b;
 
-int printPrime(int n)
-{
-    for (int i = 2; i <= n; i++)
+    // cout << a << "  " << b << "  ";
+    for (int i = 0; i < n - 2; i++)
     {
-        bool primeChecker = isPrime(i);
+        next = a + b;
 
-        if (primeChecker == true) {
-            cout << i << " is a Prime number" << endl;
-        }
+        a = b;
+        b = next;
+        // cout << next << "  ";
     }
+    return next;
 }
 
 int main()
 {
-    cout << "1 is a Prime number" << endl;
-    cout << printPrime(15);
+
+    cout << fibo(10);
     return 0;
 };
