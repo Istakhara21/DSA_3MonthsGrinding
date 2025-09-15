2d8891a1ef539e71d6ebffe9995e172f992b88f5
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-15 11:19:26 +0530

    WAF to calculate sum of n integers

--- code.cpp
+++ code.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 // to calculate sum of n digits of a number
-int sumOfDigit(int num) {
-    int digSum = 0;
+int factorial(int n) {
+    int sum = 1;
 
-    while(num > 0) {
-        int lastDig = num % 10;
-        num /= 10;
-        digSum += lastDig;
+    while(n>0) {
+        sum = sum*n;
+        n--;
     }
-    return digSum;
+    return sum;
+}
+
+int nCr(int n, int r) {
+    int val = factorial(n) / ((factorial(r) * factorial(n-r)));
 }
 
 int main () {
-    cout << "sum = " << sumOfDigit(2356) << endl;
+    cout << "nCr = " << nCr(6,3) << endl;
     return 0;
 }
