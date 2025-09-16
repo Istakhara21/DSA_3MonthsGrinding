6bc8b52b620fe94db05c20a32d4bdfd30891f803
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-16 11:53:21 +0530

    I tried to find 2's complement but got stuck at third step which is adding one bit.

--- code.cpp
+++ code.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 using namespace std;
 
-// Binary to Decimal
-int DecToBin(int n)
-{
-    int sum = 0, pow = 1; // pow =1 -> 10(0)
-    while (n > 0)
-    {
-        int rem = n % 2;
-        sum += (rem * pow);
+int DecToBin(int n) {
+    int bin = 0;
+    int pow = 1;
+    while(n>0) {
+        int rem = n%2;
+        bin += rem*pow;
         pow *= 10;
         n /= 2;
     }
-    return sum;
+    return bin;
 }
 
-int main()
-{
+int flipBit(int x) {
+    int bin = DecToBin(x);
+    int pow = 1;
+    //most updated pow
+    while(bin>0) {
+        bin /= 10;
+        pow *= 10;
+    }
+    bin = DecToBin(x);
+    int reverseBit = 0;
+    while(bin>0) {
+        int lastDig = bin%10;
+        reverseBit += lastDig*pow;
+        pow /= 10;
+        bin /= 10;
+    }
+    return reverseBit;
+}
 
-    cout << DecToBin(55);
-    return 0;
-};
+int main() {
+    cout << flipBit(55) + 1 ;
+    // return 0;
+}
