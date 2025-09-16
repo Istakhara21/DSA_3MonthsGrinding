80305dcc3f4c9ec58abe4b76b2e823ad0a883f7f
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-16 14:15:41 +0530

    reverse the decimal number

--- code.cpp
+++ code.cpp
@@ -1,38 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int DecToBin(int n) {
-    int bin = 0;
-    int pow = 1;
-    while(n>0) {
-        int rem = n%2;
-        bin += rem*pow;
-        pow *= 10;
-        n /= 2;
+int reverseNum(int n)
+{
+    int newNum = 0;
+    while (n > 0)
+    {
+        int lastDig = n % 10;           // take last digit
+        newNum = newNum * 10 + lastDig; // build reversed number
+        n = n / 10;                     // remove last digit
     }
-    return bin;
+    return newNum;
 }
 
-int flipBit(int x) {
-    int bin = DecToBin(x);
-    int pow = 1;
-    //most updated pow
-    while(bin>0) {
-        bin /= 10;
-        pow *= 10;
-    }
-    bin = DecToBin(x);
-    int reverseBit = 0;
-    while(bin>0) {
-        int lastDig = bin%10;
-        reverseBit += lastDig*pow;
-        pow /= 10;
-        bin /= 10;
-    }
-    return reverseBit;
-}
-
-int main() {
-    cout << flipBit(55) + 1 ;
-    // return 0;
+int main()
+{
+    cout << reverseNum(125);
+    return 0;
 }
