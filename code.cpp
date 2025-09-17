64403eddec80f225d5d2bbf4937cb228bf8fc511
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-17 12:27:13 +0530

    finding smallest and largest number in an array

--- code.cpp
+++ code.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int reverseNum(int n)
-{
-    int newNum = 0;
-    while (n > 0)
-    {
-        int lastDig = n % 10;           // take last digit
-        newNum = newNum * 10 + lastDig; // build reversed number
-        n = n / 10;                     // remove last digit
-    }
-    return newNum;
-}
+
 
 int main()
 {
-    cout << reverseNum(125);
+    int arr[6] = {5,15,22,1,-5,24};
+    //find the smallest number
+    int smallest = INT_MAX;
+    for(int i=0; i<6; i++) {
+        if(smallest > arr[i]) {
+            smallest = arr[i];
+        }
+    }
+    cout << smallest << endl;
+
+    //TO find the largets number
+    int largest = INT_MIN;
+    for(int i=0; i<6; i++) {
+        if(largest < arr[i]) {
+            largest = arr[i];
+        }
+    }
+    cout << largest;
     return 0;
 }
