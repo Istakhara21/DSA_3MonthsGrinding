4dafede59e4c0c3845077dddfcecf406b8c1f936
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-18 12:47:07 +0530

    linear search for vector

--- code.cpp
+++ code.cpp
@@ -3,25 +3,18 @@
 #include <vector>
 using namespace std;
 
-int linearSearch(int arr1[], int sz)
-{
-    int target = 8;
-    for(int i=0; i<sz; i++) {
-        if(target == arr1[i]) {
-            cout << "It is in the array " << arr1[i] <<  endl;
-            cout << "Index No: " << i << endl;
-        }
-    }
-    return 0;
-}
-
 int main()
 {
-    int arr1[] = {1,2,3,8,4,5};
-    // int arr2[] = {3,25,12,2,22};
-    int sz = 5;
+    vector<int> vec = {1, 2, 34, 5, 8, 342, 9343};
 
-    cout << linearSearch(arr1, sz);
+    int target = 8;
+    for (int val : vec)
+    {
+        if (val == target)
+        {
+            cout << val << " is inside the vector" << endl;
+        }
+    }
 
     return 0;
 }
