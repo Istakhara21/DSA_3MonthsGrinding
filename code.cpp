e91e74b79e07de19dc833bfef302c9b61580df58
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-18 12:57:14 +0530

    reverse a vector

--- code.cpp
+++ code.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int main()
 {
     vector<int> vec = {1, 2, 34, 5, 8, 342, 9343};
-
+    vector<int> newVec;
+   
     int target = 8;
-    for (int val : vec)
-    {
-        if (val == target)
-        {
-            cout << val << " is inside the vector" << endl;
-        }
+    for(int i=vec.size()-1; i>=0; i--) {
+        newVec.push_back(vec[i]);
+    }
+
+    for(int val: newVec) {
+        cout << val << "  ";
     }
 
     return 0;
