cbc3ff20b2755a62f947044862e040414df5e292
Istakhar Ahmad <istakhara21@gmail.com>
2025-09-18 11:59:16 +0530

    swap min and max of an array [Little confused]

--- code.cpp
+++ code.cpp
@@ -3,18 +3,48 @@
 #include <vector>
 using namespace std;
 
-int sumProd(int arr[], int sz)
+int largestVal(int arr[], int sz)
 {
-    int sum = 0;
-    int prod = 1;
+    int largest = INT_MIN;
+
     for (int i = 0; i < sz; i++)
     {
-        sum += arr[i];
-        prod *= arr[i];
+        if (largest < arr[i])
+        {
+            largest = arr[i];
+        }
     }
+    cout << "Largest value: " << largest << endl;
+    
+}
 
-    cout << "sum: " << sum << endl;
-    cout << "Product: " << prod << endl;
+int SmallestVal(int arr[], int sz)
+{
+    int smallest = INT_MAX;
+
+    for (int i = 0; i < sz; i++)
+    {
+        if (smallest > arr[i])
+        {
+            smallest = arr[i];
+        }
+    }
+    cout << "Smallest value: " << smallest << endl;
+}
+
+int swapFn(int arr[], int sz) {
+    int min = SmallestVal(arr, sz);
+    int max = largestVal(arr, sz);
+
+    for(int i=0; i<sz; i++) {
+        if(arr[i] == min) {
+            swap(max, min);
+            break;
+        }
+    }
+    for (int i=0; i<sz; i++) {
+        cout << arr[i] << " ";
+    }
 }
 
 int main()
@@ -22,7 +52,13 @@ int main()
     int arr[] = {34, 45, 34, 2, 4, 23, 142};
     int sz = 7;
 
-    cout << sumProd(arr, sz) << endl;
-    
+    cout << "Original array: " << endl;
+    for (int i=0; i<sz; i++) {
+        cout << arr[i] << " " ;
+    }
+    cout << endl;
+
+    cout << "New array: " << endl;
+    cout << swapFn(arr, sz);
     return 0;
 }
